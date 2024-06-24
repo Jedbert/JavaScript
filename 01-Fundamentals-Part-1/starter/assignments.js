1b8b0095cfbc2b592c8eab515b6ebd3959cdd4f5
Jedbert <jedbert@yahoo.com>
2024-06-24 12:47:38 +0100

    12:47pm

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+let country = "Nigeria"
+let continent = "Africa"
+let population = "Over 200 million"
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,39 @@
+/*let js = 'amazing'
+console.log(40 + 8 + 23 - 10)
+
+let firstName = "Jedidiah"
+console.log(firstName)
+
+let myFirstJob = "COO at GBEFLA Nigeria Limited"
+let myCurrentJob = "Programmer"
+
+console.log(myFirstJob)
+*/
+
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun)
+console.log(typeof true)
+console.log(typeof javascriptIsFun)
+console.log(typeof 23)
+console.log(typeof "Jedidiah")
+*/
+// Mathematical Operators
+const now = 2037;
+const ageJedidiah = now - 1999;
+const ageWumi = now - 2002
+console.log(ageJedidiah, ageWumi)
+
+console.log(ageJedidiah * 2, ageJedidiah / 10, 2 ** 3)
+// 2 ** 3 means 2 to the power of 3
+
+const firstName = "Jedidiah";
+const lastName = "Olugbenga";
+console.log(firstName + " " + lastName)
+
+// Assignment operators
+let x = 10 + 5;
+x += 10; //x = x + 10 = 25
+console.log(x);
+
+// Comparison Operators
+console.log(ageJedidiah > ageWumi); // >, <, >=, <=
